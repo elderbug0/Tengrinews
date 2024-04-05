11ab49e3edca5106624b09d65f94fe74d176759e
Beibarys <beibarys0809@mail.ru>
2024-04-05 16:01:56 +0500

    add new

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/eyes')
-def eyes():
-    return render_template('news_1.html')
+@app.route('/systema_gos')
+def corrution():
+    return render_template('new_law.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

98dd434263da34d655dfa4d89b9816b507c08e39
Beibarys <beibarys0809@mail.ru>
2024-04-05 20:20:13 +0500

    new

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ def home():
 @app.route('/systema_gos')
 def corrution():
     return render_template('new_law.html')
+@app.route('/tokaev')
+def tokaev():
+    return render_template('tokaev.html')
+@app.route('/earn')
+def earn():
+    return render_template('earn.html')
+@app.route('/aktobe')
+def aktobe():
+    return render_template('aktobe.html')
+@app.route('/bishimbaev')
+def bishimbaev():
+    return render_template('bishimbaev.html')
+@app.route('/moshennik')
+def moshennik():
+    return render_template('moshennik.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var subDiv = document.getElementById('sub');
     var logo = document.getElementById('logo');
+    var newsItems = document.querySelectorAll('.news-item'); // Select all news items
 
     function setCursor(element) {
         element.addEventListener('mouseenter', function() {
@@ -23,4 +24,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     setCursor(subDiv);
     setClickEvent(subDiv, 'https://www.whatsapp.com/channel/0029VaC8d1M3AzNVm4DlYk2H');
+
+    // New: Iterate over each news item to add cursor style and click event
+    newsItems.forEach(function(item) {
+        setCursor(item); // Apply cursor style on hover
+        // Use the data-route attribute for the URL in the click event
+        var route = item.getAttribute('data-route');
+        setClickEvent(item, route);
+    });
 });

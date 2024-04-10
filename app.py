add82200b3d869037bc8bf274f7df69813c1c5f6
Beibarys <beibarys0809@mail.ru>
2024-04-10 22:00:52 +0500

    rename

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import json
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
 db = SQLAlchemy(app)
